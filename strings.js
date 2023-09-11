f28d4a300570409c04165918b29039c2c8127c3d
AndoniUTT <andoni2020lopez@gmail.com>
2023-09-11 11:57:19 -0600

    04 Stringss

---
+++ strings.js
@@ -0,0 +1,21 @@
+//Strings en JS
+
+let myString = "Hola mundo." + 'Mi string';
+console.log(myString);
+
+console.log('I\'m And'); //usar el slash para usar la comilla dentro de las mismas
+
+console.log(myString.charAt(3)) //el caracter en la posicion
+console.log(myString.length) //numero de caracteres
+
+
+const menu = `
+1. Opcion 1
+2. Opcion 2
+3. opcion 3
+`;  //alt gr + tecla(}) + espacio
+
+console.log(menu)
+//console.log(`Hola, soy$(variable) y soy un $(variable)`)
+
+ 
