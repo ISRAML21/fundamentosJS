f04bc52621ac5890497ba7dc8b14725a753b68fa
AndoniUTT <andoni2020lopez@gmail.com>
2023-09-11 11:20:39 -0600

    02 Tipos de datos en JS

---
+++ datos.js
@@ -0,0 +1,12 @@
+console.log('Ejecutando desde Node');
+
+//Tipos de datos de JavaScript
+
+console.log(12);
+console.log("Este es string");
+let variable=1;   //cuidado con asignar palabras reservadas a los nombres de variables
+console.log(variable);
+console.log(typeof(variable));
+console.log([]);
+console.log(typeof([]));
+
